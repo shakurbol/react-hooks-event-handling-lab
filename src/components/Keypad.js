aa1b1409106a00d8d729505ebef154bcb17f4939
shakurbol <bolakokbol@gmail.com>
2023-11-07 09:05:57 +0300

    assingment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
 // Code Keypad Component Here
 
-function Keypad (){
+import React from "react";
+
+class Keypad extends React.Component {
+  handleChange = () => {
+    console.log("Entering password...");
+  };
+
+  render() {
     return (
-        <div></div>
-    )
+      <div>
+        <input type="password" onChange={this.handleChange} />
+      </div>
+    );
+  }
 }
 
 export default Keypad;
